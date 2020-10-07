e09904b756003624d0d565520f807008e29125ba
sathapornma <sathaporn.ma@rmuti.ac.th>
2020-10-07 22:44:14 +0700

    add fuction upload image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,92 @@
 import React, { Component } from 'react';
+import axios from 'axios' ;
 
 export default class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { data: [] };
+
+
+  state = {
+    name: '',
+    description: '',
+    avatar: null,
+    heros: [],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+
+  handleImageChange = (e) => {
+    this.setState({
+      avatar: e.target.files[0]
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    
+    let form_data = new FormData();
+    form_data.append('name', this.state.name);
+    form_data.append('description', this.state.description);
+    form_data.append('avatar', this.state.avatar, this.state.avatar.name);
+    
+    let url = 'http://localhost:8000/api/post/';
+
+    axios.post(url, form_data, {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err))
+
+  };
+
+  componentDidMount(){
+    let url = 'http://localhost:8000/api/post/';
+    axios.get(url)
+      .then(res => {
+        const all_heros = res.data;
+        this.setState({ heros : all_heros });
+      })
+      .catch(err => console.log(err))
+
   }
 
-  async componentDidMount() {
-    const response = await fetch('http://127.0.0.1:8000/api/');
-    const json = await response.json();
-    this.setState({ data: json });
-    console.log(this.state.data);
-  }
+  render() {
+    let url = 'http://localhost:8000';
 
-  
-  render(){
     return (
-      <div>
-        {this.state.data.map((hero)=>
-          <div key={hero.id}>
-            <h5>{hero.name}</h5>
-            <p>{hero.description}</p>
-          </div>
-        )}
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="text" placeholder='name' id='name' value={this.state.name} onChange={this.handleChange} required/>
+          </p>
+          <p>
+            <input type="text" placeholder='description' id='description' value={this.state.description} onChange={this.handleChange} required/>
+
+          </p>
+          <p>
+            <input type="file" id="avatar" accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
+          </p>
+          <input type="submit"/>
+        </form>
+
+        <div>
+          {this.state.heros.map((hero)=>
+            <div key={hero.id}>
+              <img src={url + hero.avatar} alt="avatar"/>
+              <b>{hero.name}</b>
+              <p>{hero.description}</p>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
-  
 }
